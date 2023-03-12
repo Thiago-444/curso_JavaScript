cd953ccbbfd380f5ef239ad82a4c9fe2c1a4a2e9
Thiago <03thiago.campos@gmail.com>
2023-03-11 21:57:30 -0400

    arquivo JS

---
+++ main.js
@@ -0,0 +1,11 @@
+const nome = 'Thiago' // string
+const num1 = 44 //Number
+const num2 = 44.4 // Number
+let v; // undefined -> nao aponta local nenhum na memoria
+const nulo = null; //nao aponta local nenhum na memoria
+const boolean = false //valor logico
+
+console.log(typeof nome) //verificando o tipo da variavel
+
+console.log(`O tipo da variavel nome é ${typeof nome}`) // verificando
+//o tipo da variavel nome usando typeof dentro de template string
