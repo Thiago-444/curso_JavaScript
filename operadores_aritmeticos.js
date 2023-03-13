104d5f062c96a26aa6be687b1b87bdafa6e80dda
Thiago <03thiago.campos@gmail.com>
2023-03-12 20:27:28 -0400

    add codigo em operadores_aritmeticos ate row9

--- operadores_aritmeticos.js
+++ operadores_aritmeticos.js
@@ -1,5 +1,11 @@
 const num = 16
+const num_2 = 81
 
 const duplica = num * 2 //resultado = 32
 const expoente = num ** 2 //num elevado a 2, resultado = 256
+
+// Raiz quadrada de um numero
+const raiz = num ** (1/2) //resultado = 4 ou...
+const raiz_2 = num ** 0.5 //resultado = 4
+
 
