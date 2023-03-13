acf9416b4c223c6881798215e2aa16f210a0309d
Thiago <03thiago.campos@gmail.com>
2023-03-12 21:31:24 -0400

    add codigo em operadores_aritmeticos ate row 17

--- operadores_aritmeticos.js
+++ operadores_aritmeticos.js
@@ -1,5 +1,5 @@
 const num = 16
-const num_2 = 81
+let num_2 = 81
 
 const duplica = num * 2 //resultado = 32
 const expoente = num ** 2 //num elevado a 2, resultado = 256
@@ -8,4 +8,11 @@ const expoente = num ** 2 //num elevado a 2, resultado = 256
 const raiz = num ** (1/2) //resultado = 4 ou...
 const raiz_2 = num ** 0.5 //resultado = 4
 
+const resto = 10 % 5  //o resto da divisao é zero
+const resto_2 = 9 % 2 // o resto é um
+
+//Operador de incremento
+num_2++; //resultado = 82
+num_2++; //resultado = 83
+num_2++; //resultado = 84
 
